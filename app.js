eecf387763d57276064bc51b28323d1d4f0b9896
Preethi SV <preethisv0@gmail.com>
2020-05-13 01:57:47 +0530

    Level 3 - Hashing Passwords

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-var encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
-console.log(process.env.API_KEY);
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,9 +18,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET,  encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -41,7 +37,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err) {
@@ -55,7 +51,7 @@ app.post("/register", function(req, res) {
 app.post("/login", function(req, res) {
 
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email:username}, function(err, foundUser) {
     if (err) {
@@ -68,9 +64,8 @@ app.post("/login", function(req, res) {
       }
     }
   });
-
-
 });
+
 app.listen(3000, function() {
     console.log("Server started on port 3000.");
 });
