7e34dbb62d2f46d5f63a5ef0e15db943dadf5c4f
Preethi SV <preethisv0@gmail.com>
2020-05-13 14:52:28 +0530

    Level 6 - Google OAuth 2.0 Authentication

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose");
 const session = require('express-session'); // Step 1
 const passport = require('passport'); // Step 1
 const passportLocalMongoose = require("passport-local-mongoose"); // Step 1
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // Step 10
+const findOrCreate = require('mongoose-findorcreate'); // Step 13
 
 const app = express();
 
@@ -14,15 +16,15 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Note - On updating code, the server restarts and cookie is deleted in server
-// Step 2 - Place app.use(session()) in this exact location
+// Note - On updating code, the server restarts and cookie is deleted in server.
+// Step 2 - Place app.use(session()) in this exact location.
 app.use(session({ // Step 2
   secret: "Our long little string",
   resave: false,
   saveUninitialized: false,
 }));
 
-// Step 3 - After Step 2 - Place this
+// Step 3 - After Step 2, Place this.
 app.use(passport.initialize()); // Step 3
 app.use(passport.session()); // Step 3
 
@@ -31,23 +33,73 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  // Step 18 - Each time, when user signs in with user, we save google id
+  // to log in the user next time, instead of creating new user again.
+  googleId: String // Step 18
 });
 
-//Step 4 - Add plugin to Schema
+// Step 4 - Add plugin to Schema.
 userSchema.plugin(passportLocalMongoose); // Step 4
+// Step 14 - Add mongoose-findorcreate plugin.
+userSchema.plugin(findOrCreate); // Step 14
 
 const User = mongoose.model("User", userSchema);
 
 // Step 5 - use local strategy
 passport.use(User.createStrategy()); // Step 5
 
-// Step 5 - use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// Step 5 - use static serialize and deserialize of model for passport session support.
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+// Step 17 - Common serializing and deserializing (session support) by passport
+// that works for all strategies (local, google, etc).
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// Step 11 - Use Google to authenticate
+passport.use(new GoogleStrategy({ // Step 11
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    // Step 12 - User.findOrCreate is not a function of mongoose.
+    // We need Step 13 & 14 for this to work.
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      // Step 18 - We find googleId in Database, if it exists, login
+      // else create a new user and then login the new user.
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res){
   res.render("home");
+});
+
+// Step 15 - Get route when user clicks on Sign in with Google.
+// passport authenticates with Google and scope is what user info we get back.
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] }));
+
+// step 16 - After authentication, Google redirects the user to this callback.
+app.get("/auth/google/secrets",
+  // If auth fails, redirect to login.
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect("/secrets");
 });
 
 app.get("/login", function(req, res){
